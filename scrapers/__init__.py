5c16a76db9cb38de73abe41096e675c2bcbb7cda
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-01-21 21:10:50 +0530

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -11,7 +11,7 @@ __all__ = ['BaseScraper']
 
 # scrapers/anime/__init__.py
 """
-Anime scrapers package
+Anime scrapers package - Complete
 File: scrapers/anime/__init__.py
 """
 
@@ -19,12 +19,26 @@ from .anidb_scraper import AniDBScraper
 from .anilist_scraper import AniListAnimeScraper
 from .myanimelist_scraper import MyAnimeListAnimeScraper
 from .kitsu_scraper import KitsuAnimeScraper
+from .simkl_scraper import SIMKLAnimeScraper
+from .animenewsnetwork_scraper import AnimeNewsNetworkScraper
+from .animeplanet_scraper import AnimePlanetScraper
+from .livechart_scraper import LivechartScraper
+from .themoviedb_scraper import TMDBAnimeScraper
+from .tvdb_scraper import TVDBScraper
+from .imdb_scraper import IMDBScraper
 
 __all__ = [
     'AniDBScraper',
     'AniListAnimeScraper',
     'MyAnimeListAnimeScraper',
-    'KitsuAnimeScraper'
+    'KitsuAnimeScraper',
+    'SIMKLAnimeScraper',
+    'AnimeNewsNetworkScraper',
+    'AnimePlanetScraper',
+    'LivechartScraper',
+    'TMDBAnimeScraper',
+    'TVDBScraper',
+    'IMDBScraper'
 ]
 
 
@@ -34,4 +48,12 @@ Manga scrapers package
 File: scrapers/manga/__init__.py
 """
 
-__all__ = []
+from .anilist_scraper import AniListMangaScraper
+from .myanimelist_scraper import MyAnimeListMangaScraper
+from .kitsu_scraper import KitsuMangaScraper
+
+__all__ = [
+    'AniListAnimeScraper',
+    'MyAnimeListAnimeScraper',
+    'KitsuAnimeScraper'
+]
